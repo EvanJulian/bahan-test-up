1db224c92d78191d831e56e162c08329918bfcf7
aldrichsancho <86425287+EvanJulian@users.noreply.github.com>
2023-02-10 14:25:39 +0700

    rubah warna

--- ujian_perbaikan_2073008/lib/FirstPage.dart
+++ ujian_perbaikan_2073008/lib/FirstPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ujian_perbaikan_2073008/SecondPage.dart';
+
+import 'BlocProvider.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -9,12 +12,14 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
+  
   @override
   Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage()));
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage(bloc: bloc,)));
             },
             child: Icon(Icons.settings)
           ),
@@ -22,19 +27,22 @@ class _FirstPageState extends State<FirstPage> {
         title: Text('Halaman Data Diri'),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.only(top:30.0),
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [Text('NRP : '), Text('2073008')],
-              ),
-              Row(
-                children: [Text('Nama : '), Text('Evan Julian Sudarman')],
-              )
-            ],
+      body: BlocBuilder<ColorBloc, Color>(
+        builder: (context, currentColor) => 
+        Padding(
+          padding: const EdgeInsets.only(top:30.0),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [Text('NRP : ', style: TextStyle(color: currentColor),), Text('2073008', style: TextStyle(color: currentColor))],
+                ),
+                Row(
+                  children: [Text('Nama : ', style: TextStyle(color: currentColor)), Text('Evan Julian Sudarman', style: TextStyle(color: currentColor))],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- ujian_perbaikan_2073008/lib/SecondPage.dart
+++ ujian_perbaikan_2073008/lib/SecondPage.dart
@@ -1,15 +1,112 @@
 import 'package:flutter/material.dart';
 
+import 'BlocProvider.dart';
+
 class SecondPage extends StatefulWidget {
-  const SecondPage({ Key? key }) : super(key: key);
+  final ColorBloc bloc;
+  const SecondPage({Key? key, required this.bloc}) : super(key: key);
 
   @override
   State<SecondPage> createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
+  TextEditingController namaController = TextEditingController();
+  TextEditingController nrpController = TextEditingController();
+
+  String selectedValue = "Black";
+
+  List _color = ['Black', 'Red', 'Blue'];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(children: [
+          TextFormField(
+            controller: nrpController,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Color(0XFFA1A1A1)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Color(0XFF4F3E9C), width: 2),
+              ),
+              labelText: 'NRP',
+              hintText: 'NRP',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            controller: namaController,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Color(0XFFA1A1A1)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Color(0XFF4F3E9C), width: 2),
+              ),
+              labelText: 'Nama',
+              hintText: 'Nama',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownButton(
+            hint: Text("Select The Gender"),
+            value: selectedValue,
+            items: _color.map((value) {
+              return DropdownMenuItem(
+                child: Text(value),
+                value: value,
+              );
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                selectedValue = value.toString();
+              });
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: (){
+              var tmp;
+              if(selectedValue == 'Black'){
+                tmp = ColorEvent.to_black;
+              }
+              else if(selectedValue == 'Red'){
+                tmp = ColorEvent.to_red;
+              }
+              else if(selectedValue == 'Blue'){
+                tmp = ColorEvent.to_blue;
+              }
+              widget.bloc.add(tmp);
+              Navigator.pop(context);
+            },
+            child: Center(
+              child: Container(
+                height: 50,
+                width: 200,
+                color: Colors.blue,
+                child: Text('Simpan'),
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
